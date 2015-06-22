4c40aa2c1f2cfc8af462452d5b80252e9888f65d
yg <yog16patel@gmail.com>
2015-06-22 21:31:15 +0530

    First commit to check it is

---
+++ app/src/main/java/com/citymart/yg/materialdesigndemo/Information.java
@@ -0,0 +1,26 @@
+package com.citymart.yg.materialdesigndemo;
+
+/**
+ * Created by yg on 6/21/2015.
+ */
+public class Information
+{
+    private  int iconId;
+    private String tittle;
+
+    public int getIconId() {
+        return iconId;
+    }
+
+    public void setIconId(int iconId) {
+        this.iconId = iconId;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+}
